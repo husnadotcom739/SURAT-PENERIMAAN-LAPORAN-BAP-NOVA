3261fd1b9ef8fabcba6f70b0df6f4277e00f52d6
husnadotcom739 <husnadotcom739@gmail.com>
2026-03-20 16:56:24 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Pastikan ini sama persis dengan nama repo di URL GitHub Anda
-  base: '/SURAT-PENERIMAAN-LAPORAN/', 
+  base: './', 
   build: {
     outDir: 'dist',
   }
 })
+
