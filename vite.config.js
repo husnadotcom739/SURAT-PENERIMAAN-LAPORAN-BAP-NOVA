f8910580cf58d841bb1a2e0b9e91e9d9b687455b
husnadotcom739 <husnadotcom739@gmail.com>
2026-03-20 16:51:25 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  // Ganti base menjadi path subfolder di domain Anda
-  base: '/SURAT-PENERIMAAN-LAPORAN-BAP-NOVA/', 
+  base: '/', // Kembali ke root agar aset terpanggil di domain utama
   build: {
     outDir: 'dist',
   }
